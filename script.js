bf57d4a084e3033d673e2a6bd62d0a91bdc2e002
Tejas Sahebrao Gawali <156706283+TejasSahebraoGawali@users.noreply.github.com>
2026-01-01 11:29:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+const contianer = document.querySelector(".container");
+
+for(let i = 1; i <= 800; i++) {
+	let square = document.createElement("section");
+	square.classList.add("square");
+	
+	square.addEventListener("mouseenter", ()=>{
+		setColor(square);
+	});
+
+	square.addEventListener("mouseleave", () => {
+    resetColor(square);
+	});
+	
+	contianer.appendChild(square);
+}
+
+function generateRandomColor() {
+	return `rgb(${Math.floor(Math.random()*256)},
+				${Math.floor(Math.random()*256)},
+				${Math.floor(Math.random()*256)})`;
+}
+
+function setColor(square) {
+	square.style.backgroundColor = generateRandomColor();
+}
+
+function resetColor(square) {
+	setTimeout(()=>{
+	square.style.backgroundColor = "gray"
+}, 1000);
+}
+
+	
